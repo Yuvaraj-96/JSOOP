7186e6b156e606a2526b6f244ee7be312d031f84
Yuvaraj <87924193+Yuvaraj-96@users.noreply.github.com>
2022-01-17 00:08:36 +0530

    Added 3 rd condition

--- movieclass.js
+++ movieclass.js
@@ -10,9 +10,26 @@ class movie{
         this.title = title;
         this.studio = studio;
     }
-    
+
+    getPG(moviesarr)
+{
+
+   let ret= moviesarr.filter(movie1=>movie1.rating === 'PG');
+       
+ return ret;
 }
 
+    
+}
+const moviesar = [ { title: 'Casino Royale', studio: 'Eon Productions', rating: 'PG' }, { title: 'Casino', studio: 'Eon', rating: 'PG'}, { title: 'Royale', studio: '20th Century fox  film', rating: 'AT' }, { title: 'CasiRoyale',studio: 'Quincy', rating: 'PG' }, { title: 'CasRoyale',studio: 'Quincy', rating: 'PG' } ];
 const Casino = new movie("Casino Royale","Eon Productions","PG");
+
+console.log(Casino.getPG(moviesar));
 console.log(Casino);
+
+
+
+
+
+
 
